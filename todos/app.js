6d675f943d375cbbd3ca978a415aabf47c8e178d
bharathithal <98535307+bharathithal@users.noreply.github.com>
2022-02-11 01:04:00 +0530

    Add files via upload

---
+++ todos/app.js
@@ -0,0 +1,53 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
+
+const generateTemplete = (todo) => {
+
+    const html = `<li class="list-group-item d-flex justify-content-between align-items-center">
+                    <span>${todo}</span>
+                    <i class="far fa-trash-alt delete"></i>
+                </li>`;
+
+    list.innerHTML += html;
+};
+
+addForm.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+    const todo = addForm.add.value.trim();
+    if(todo.length) {
+        generateTemplete(todo);
+        addForm.reset();
+    }
+});
+
+//delete todods
+list.addEventListener('click', (e) => {
+
+    if(e.target.classList.contains('delete')) {
+
+        e.target.parentElement.remove();
+    }
+});
+
+const filterTodos = (term) => {
+
+    Array.from(list.children)
+        .filter(todo => !todo.textContent.toLowerCase().includes(term))
+        .forEach((todo) => todo.classList.add('filtered'))
+
+    Array.from(list.children)
+        .filter(todo => todo.textContent.toLowerCase().includes(term))
+        .forEach((todo) => todo.classList.remove('filtered'))
+};
+
+//key up
+search.addEventListener('keyup', () => {
+
+    const term = search.value.toLowerCase().trim();
+    if(filterTodos(term)) {
+
+
+    }
+})
